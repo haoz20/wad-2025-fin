b169f7fac88b62da149fb782033433d31d31a978
haoz20 <zhaonianhao2005@gmail.com>
2025-10-02 10:55:36 +0700

    create Customer schema

---
+++ models/Customer.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+
+const customerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    memberNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    interests: {
+        type: String,
+        required: false
+    }
+}, {
+    timestamps: true
+})
+
+const Customer = mongoose.models.Customer || mongoose.model("Customer", customerSchema);
+
+export default Customer;
